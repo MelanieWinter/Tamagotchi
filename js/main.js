55db5e0fb62e324e6cc76fa1d18de17098dd19cb
Melanie Winter <melaniebucher830@gmail.com>
2023-11-04 02:02:02 -0700

    Refactors code with MVC structure

--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
-const name = document.getElementById('name')
-const petAgeEl = document.getElementById('age')
-const petHome = document.getElementById('pet-home')
-const petImage = document.getElementById('pet')
-const petHungerEl = document.getElementById('pet-hunger-span')
-const petSleepinessEl = document.getElementById('pet-sleepiness-span')
-const petBoredomEl = document.getElementById('pet-boredom-span')
-const feed = document.getElementById('feed')
-const lights = document.getElementById('lights')
-const play = document.getElementById('play')
-const petText = document.getElementById('pet-text')
+// CONSTANTS //
 
-let areLightsOn = true
+// STATE VARIABLES //
+let time = 0
+let gameRunning = true
 
+// CACHED ELEMENTS - VIEW //
+const petNameEl = document.getElementById('name-el')
+const petAgeEl = document.getElementById('pet-age-span-el')
+const petHomeEl = document.getElementById('pet-home-el')
+const petImageEl = document.getElementById('pet-el')
+const petHungerEl = document.getElementById('pet-hunger-span-el')
+const petSleepinessEl = document.getElementById('pet-sleepiness-span-el')
+const petBoredomEl = document.getElementById('pet-boredom-span-el')
+const feedButtonEl = document.getElementById('feed-button-el')
+const sleepButtonEl = document.getElementById('sleep-button-el')
+const playButtonEl = document.getElementById('play-button-el')
+const petTextEl = document.getElementById('pet-text-el')
+const nameInputEl = document.getElementById('name-input-el')
+const nameInputButtonEl = document.getElementById('name-input-button-el')
+const nameInputDivEl = document.getElementById('name-input-div-el')
+
+// OBEJECT - MODEL //
 class Pet {
-    constructor({name, age, ageEl, home, hunger, hungerMetric, sleepiness, sleepinessMetric, boredom, boredomMetric, image, textBox}) {
+
+    // MODEL //
+    constructor({name, nameEl, age, ageEl, homeEl, hungerMetricEl, sleepinessMetricEl, boredomMetricEl, imageEl, textBoxEl}) {
         this.isAlive = true;
         this.isIdle = true;
         this.isAnimated = false;
         this.name = name;
+        this.nameEl = nameEl;
         this.age = age;
-        this.ageEl = ageEl
-        this.home = home
-        this.hungerMetric = hungerMetric;
-        this.sleepinessMetric = sleepinessMetric;
-        this.boredomMetric = boredomMetric;
-        this.image = image;
-        this.textBox = textBox;
-        this.metrics = {
-            hunger: 0,
-            sleepiness: 0,
-            boredom: 0,
-        };
+        this.ageEl = ageEl;
+        this.homeEl = homeEl;
+        this.hungerMetricEl = hungerMetricEl;
+        this.sleepinessMetricEl = sleepinessMetricEl;
+        this.boredomMetricEl = boredomMetricEl;
+        this.imageEl = imageEl;
+        this.textBoxEl = textBoxEl;
+        this.metrics = [
+            { name: 'hunger', value: 0 },
+            { name: 'sleepiness', value: 0 },
+            { name: 'boredom', value: 0 }
+        ];
         this.animations = [
             this.jello,
             this.heartbeat,
@@ -40,192 +52,129 @@ class Pet {
         this.currentAnimationIdx = 0
     }
 
-    dead = () => {
-        if (!this.isAlive) {
-            this.image.style.backgroundColor = 'black'
-            this.home.style.filter = 'blur(1.5rem)'
-        }
+    // CONTROLLER //
+    increaseAge() {
+        this.age++
+        this.render()
+        console.log('AGE: ', this.age)
     }
 
-    increaseAge = () => {
-        const updateAge = () => {
-            this.age++
-            this.ageEl.innerText = this.age
-            setTimeout(updateAge, 30000)
-        }
-        setTimeout(updateAge, 30000)
-    }
-    
-    updateTextBox = (text) => {
-        this.textBox.innerText = text
-        this.textBox.classList.remove('slide-out')
-        void this.textBox.offsetWidth
-        this.textBox.classList.add('slide-out')
-    }
-
-    increaseMetric = (property) => {
-        if (this.metrics[property] >= 2) {
-            this.isAlive = false
-            console.log(`${this.name} has died of ${property}.`)
-            return
-        }
-        const updateMetric = () => {
-            this.metrics[property]++
-            this[property + 'Metric'].innerText = this.metrics[property]
-            console.log('THIS: ' + property + 'Metric')
-            console.log('DEBUG: ' + property + ' = ' + this.metrics[property])
-            const randomDelay = Math.floor(5000 + Math.random() * 10000)
-            setTimeout(updateMetric, randomDelay)
-        }
-        const initialDelay = Math.floor(5000 + Math.random() * 10000)
-        setTimeout(updateMetric, initialDelay)
-    }
-
-    increaseHunger = () => {
-        this.increaseMetric('hunger')
-    }
-
-    increaseSleepiness = () => {
-        this.increaseMetric('sleepiness')
-    }
-
-    increaseBoredom = () => {
-        this.increaseMetric('boredom')
-    }
-
-    decreaseMetric = (property) => {
-        if (this.metrics[property] > 0) {         
-            this.metrics[property]--
-            this[property + 'Metric'].innerText = this.metrics[property]
-        }
-    }
-
-    feed = () => {
-        feed.addEventListener('click', () => {
-            this.decreaseMetric('hunger', this.hunger)
-        })
-    }
-
-    sleep = () => {
-        const toggleLights = () => {
-            if (areLightsOn) {
-                this.home.style.backgroundColor = 'dimgrey'
-                areLightsOn = false
-
-                const updateText = (count) => {
-                    if (count >= 1) {
-                        this.updateTextBox(count)
-                        setTimeout(() => {
-                            updateText(count - 1)
-                        }, 1000)
-                    } else {
-                        this.decreaseMetric('sleepiness', this.sleepiness)
-                    }
-                }
-                updateText(3)
-            } else {
-                this.home.style.backgroundColor = 'white'
-                areLightsOn = true
+    increaseMetric() {
+        for (let i = 0; i < this.metrics.length; i++) {
+            if(randomChance()) {
+                this.metrics[i].value++
+                this.render()
+                console.log(`${this.metrics[i].name}`, this.metrics[i].value)
             }
-        }       
-        lights.addEventListener('click', toggleLights)
-    }
-
-    play = () => {
-        play.addEventListener('click', () => {
-            this.decreaseMetric('boredom', this.boredom)
-        })
-    }
-
-    clickToAnimate = () => {
-        this.image.addEventListener('click', () => {
-            if (this.currentAnimationIdx < this.animations.length) {
-                const currentAnimation = this.animations[this.currentAnimationIdx]
-                currentAnimation()
-                this.currentAnimationIdx++
-            } else {
-                this.currentAnimationIdx = 0
-                const currentAnimation = this.animations[this.currentAnimationIdx]
-                currentAnimation()
-                this.currentAnimationIdx++
-            }
-        })
-    }
-
-    jello = () => {
-        if (!this.isAnimated ) {
-            this.isAnimated = true
-            this.image.classList.add('jello')
-            this.updateTextBox("'jiggle'")
-            setTimeout(() => {
-                this.isAnimated = false
-                this.image.classList.remove('jello')
-            }, 800)
         }
     }
 
-    heartbeat = () => {
-        if (!this.isAnimated) {
-            this.isAnimated = true
-            this.image.classList.add('heartbeat')
-            this.updateTextBox("'ouch'")
-            setTimeout(() => {
-                this.isAnimated = false
-                this.image.classList.remove('heartbeat')
-            }, 800)
-        }
+    // VIEW //
+    render() {
+        this.ageEl.innerText = this.age
+        this.hungerMetricEl.innerText = this.metrics[0].value
+        this.sleepinessMetricEl.innerText = this.metrics[1].value
+        this.boredomMetricEl.innerText = this.metrics[2].value
     }
 
-    wobble = () => {
-        if (!this.isAnimated) {
-            this.isAnimated = true
-            this.image.classList.add('wobble')
-            this.updateTextBox("'ha ha'")
-            setTimeout(() => {
-                this.isAnimated = false
-                this.image.classList.remove('wobble')
-            }, 800)
-        }
-    }
-
-    rotate = () => {
-        if (!this.isAnimated) {
-            this.isAnimated = true
-            this.image.classList.add('rotate')
-            this.updateTextBox("'wheee'")
-            setTimeout(() => {
-                this.isAnimated = false
-                this.image.classList.remove('rotate')
-            }, 800)
-        }
-    }
-
-    init = () => {
-        this.increaseAge()
-        this.increaseHunger()
-        this.increaseSleepiness()
-        this.increaseBoredom()
-        this.feed()
-        this.sleep()
-        this.play()
-        this.clickToAnimate()
-        this.dead()
+    init () {
     }
 }
 
-const player = new Pet({
-    name: "YourPetName",
+const tamagotchi = new Pet({
+
+    // MODEL //
+    name: 'name',
+    nameEl: petNameEl,
     age: 0,
     ageEl: petAgeEl,
-    home: petHome,
-    hungerMetric: petHungerEl,
-    sleepinessMetric: petSleepinessEl,
-    boredomMetric: petBoredomEl,
-    image: petImage,
-    textBox: petText,
+    homeEl: petHomeEl,
+    hungerMetricEl: petHungerEl,
+    sleepinessMetricEl: petSleepinessEl,
+    boredomMetricEl: petBoredomEl,
+    imageEl: petImageEl,
+    textBoxEl: petTextEl,
 })
 
-player.init()
+// EVENT LISTENERS - CONTROLLER //
 
-console.log(player.isAlive)
-console.log(player.hungerMetric)
+// FUNCTIONS - VIEW //
+function dead() {
+    if(!tamagotchi.isAlive) {
+        tamagotchi.imageEl.style.backgroundColor = 'black'
+        feedButtonEl.style.cursor = 'not-allowed'
+        sleepButtonEl.style.cursor = 'not-allowed'
+        playButtonEl.style.cursor = 'not-allowed'
+        tamagotchi.hungerMetricEl.innerText = '  ---'
+        tamagotchi.sleepinessMetricEl.innerText = '  ---'
+        tamagotchi.boredomMetricEl.innerText = '  ---'
+        tamagotchi.ageEl.innerText = '  ---'
+        tamagotchi.imageEl.style.cursor = 'not-allowed'
+        tamagotchi.textBoxEl.innerText = "' dead '"
+
+        // Initilize a dead tamagotchi screen with player stats
+        // Dead screen will show a different message depending on how they died
+        for (let i = 0; i < tamagotchi.metrics.length; i++) {
+            if (tamagotchi.metrics[i].value === 10) {
+                console.log(`Your tamagotchi died of ${tamagotchi.metrics[i].name}, at age ${tamagotchi.age}`)
+            }
+        }
+        if (tamagotchi.age === 60) {
+            console.log(`Your tamagotchi died at age ${tamagotchi.age}`)
+        }
+    }
+}
+
+// FUNCTIONS - CONTROLLER //
+function randomChance() {
+    const randomNum = Math.floor(1 + Math.random() * 3)
+    if (randomNum === 3) {
+        return true
+    } return false
+}
+
+function init() {
+    const game = setInterval(() => {
+        time++
+        console.log(time)
+    
+        if (time % 60 === 0) {
+            if (tamagotchi.age < 60) {
+                tamagotchi.increaseAge()
+            } else {
+                tamagotchi.isAlive = false
+                gameRunning = false
+                dead()
+                clearInterval(game)
+            }
+        }
+        
+        if (time % 2 === 0) {
+            if (tamagotchi.metrics[0].value < 10 
+            && tamagotchi.metrics[1].value < 10 
+            && tamagotchi.metrics[2].value < 10) {
+                tamagotchi.increaseMetric()
+            } else {
+                tamagotchi.isAlive = false
+                gameRunning = false
+                dead()
+                clearInterval(game)
+            }
+        }
+    }, 1000)
+
+    tamagotchi.init()
+}
+
+// FUNCTIONS  - VIEW //
+function render() {
+}
+
+// Run init only after player has named their character in the starting screen
+if (gameRunning === true) {
+    init()
+}
+
+// TO COMPLETE MVP:
+// Need to give ability to name functionality
+// Give feed, sleep, and play buttons functionality
